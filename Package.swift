f854208f51dadce76d98d7bf109604f747524329
Adam Shaw <adam@kabukivision.com>
2025-10-20 15:30:45 -0400

    Try to fix package

--- Package.swift
+++ Package.swift
@@ -9,34 +9,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PMUtils",
-            targets: ["PMCircularCollectionView"]
+            targets: ["PMUtils"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Foundation+PMUtils",
-            path: "Pod/Classes/Foundation+PMUtils",
-            publicHeadersPath: ".",
-        ),
-        .target(
-            name: "UIKit+PMUtils",
-            dependencies: ["Foundation+PMUtils"],
-            path: "Pod/Classes/UIKit+PMUtils",
-            publicHeadersPath: ".",
-        ),
-        .target(
-            name: "PMProtocolInterceptor",
-            path: "Pod/Classes/PMProtocolInterceptor",
-            publicHeadersPath: ".",
-        ),
-        .target(
-            name: "PMCircularCollectionView",
-            dependencies: ["UIKit+PMUtils", "PMProtocolInterceptor"],
-            path: "Pod/Classes/PMCircularCollectionView",
-            publicHeadersPath: ".",
 
+        .target(
+            name: "PMUtils",
+            publicHeadersPath: ".",
+            cSettings: [.headerSearchPath("PMCircularCollectionView"),.headerSearchPath("PMProtocolInterceptor"),.headerSearchPath("UIKit+PMUtils"),.headerSearchPath("Foundation+PMUtils")]
         ),
     ]
 )
